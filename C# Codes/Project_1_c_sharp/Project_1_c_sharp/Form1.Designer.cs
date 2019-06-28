30dd4b730ad598ca20c1f18a21fb98daddbe8ca7
NadaIhabAhmed <36302376+NadaIhabAhmed@users.noreply.github.com>
2019-06-28 17:19:47 +0200

    C# codes (GUI and visualization)
    Co-Authored-By: fatma abdelmoniem mohamed <fatmaabdelmoniem@users.noreply.github.com>
    Co-Authored-By: shimaa-mahmoud <shimaa-mahmoud@users.noreply.github.com>

---
+++ C# Codes/Project_1_c_sharp/Project_1_c_sharp/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Project_1_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Edges = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Edges.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(381, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of Nodes";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number of Edges";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 50);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Degree Centrality";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 50);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Closeness Centrality";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 50);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Betweeness Centrality";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(106, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(375, 205);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Edges
+            // 
+            this.Edges.Controls.Add(this.dataGridView1);
+            this.Edges.Location = new System.Drawing.Point(22, 122);
+            this.Edges.Name = "Edges";
+            this.Edges.Size = new System.Drawing.Size(505, 268);
+            this.Edges.TabIndex = 11;
+            this.Edges.TabStop = false;
+            this.Edges.Text = "Edges";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(505, 95);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nodes(n) and Edges(m)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(22, 421);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(505, 137);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Centrality Method";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Location = new System.Drawing.Point(22, 579);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(286, 318);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Centrality";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(31, 23);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 272);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 660);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Edges);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Edges.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox Edges;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ C# Codes/Project_1_c_sharp/Project_1_c_sharp/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1_c_sharp
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable edges_table = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            edges_table.Columns.Add("Edge_a", typeof(int));
+            edges_table.Columns.Add("Edge_b", typeof(int));
+            edges_table.Columns.Add("Weight", typeof(int));
+            dataGridView1.DataSource = edges_table;
+        }
+
+        
+        /*--------------------------------------Degree centrality----------------------------------------------------*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text); // nodes
+            int m = int.Parse(textBox2.Text); // edges
+
+            int[] adj_a = new int[100];
+            int[] adj_b = new int[100];
+            int[] weight_array = new int[100];
+            int[] count = new int[100];
+
+            //Datagrid values (edges)
+            for (int i = 0; i < m; i++)
+            {
+                //edge a
+                var e_a = dataGridView1.Rows[i].Cells[0].Value; // take value of cell
+                string ed_a;
+                ed_a = e_a.ToString(); // change it to string
+                int edge_a = int.Parse(ed_a);
+                //edge b
+                var e_b = dataGridView1.Rows[i].Cells[1].Value; // take value of cell
+                string ed_b;
+                ed_b = e_b.ToString();// change it to string
+                int edge_b = int.Parse(ed_b);
+                //weight
+                var w = dataGridView1.Rows[i].Cells[2].Value; // take value of cell
+                string wa;
+                wa = w.ToString(); // change it to string
+                int weight = int.Parse(wa);
+
+                adj_a[i] = edge_a;
+                adj_b[i] = edge_b;
+                weight_array[i] = weight;
+
+            }
+            //find degree of each node
+            for (int i = 0; i < m; i++)
+            {
+                count[adj_a[i]]++;
+                count[adj_b[i]]++;
+            }
+            
+
+            int[,] location = new int[22, 2] { { 500, 100 }, { 500, 200 }, { 950, 100 }, { 950, 200 }, { 700, 550 }, { 900, 500 }, { 550, 250 }, {550, 500}, {550, 430}, {910, 420},
+                                               {730, 300}, {740, 400}, { 630, 100 }, { 690, 200 }, { 640, 400 }, { 800, 200 }, { 840, 550 }, { 800, 450 }, { 800, 250 }, {860, 50},
+                                               {600, 50}, {600, 300}};
+          
+            for (int i = 0; i < m; i++)
+            {
+                draw_2_nodes_and_line(location[adj_a[i], 0], location[adj_a[i], 1], location[adj_b[i], 0], location[adj_b[i], 1], count[adj_a[i]] * 10, count[adj_b[i]] * 10, adj_a[i], adj_b[i], weight_array[i]);
+            }
+            
+            //display centrality in a text box
+            string str = "";
+            for(int o = 0; o < n; o++)
+            {
+                str += "Node" + " " + o.ToString() + " --->" + count[o] + " | ";
+            }
+            textBox3.Text = str;
+        }
+        /*--------------------------------------Closeness centrality----------------------------------------------------*/
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text); // nodes
+            int m = int.Parse(textBox2.Text); // edges
+
+            int[] adj_a = new int[100];
+            int[] adj_b = new int[100];
+            int[] weight_array = new int[100];
+            float[] count = new float[100];
+            int[,] adj_matrix = new int[100, 100];
+
+
+            //Datagrid values (edges)
+            for (int i = 0; i < m; i++)
+            {
+                //edge a
+                var e_a = dataGridView1.Rows[i].Cells[0].Value; // take value of cell
+                string ed_a;
+                ed_a = e_a.ToString(); // change it to string
+                int edge_a = int.Parse(ed_a);
+                //edge b
+                var e_b = dataGridView1.Rows[i].Cells[1].Value; // take value of cell
+                string ed_b;
+                ed_b = e_b.ToString();// change it to string
+                int edge_b = int.Parse(ed_b);
+                //weight
+                var w = dataGridView1.Rows[i].Cells[2].Value; // take value of cell
+                string wa;
+                wa = w.ToString(); // change it to string
+                int weight = int.Parse(wa);
+
+                adj_a[i] = edge_a;
+                adj_b[i] = edge_b;
+                weight_array[i] = weight;
+
+                
+            }
+
+            for (int j = 0; j < m; j++)
+            {
+                adj_matrix[adj_a[j], adj_b[j]] = weight_array[j];
+                adj_matrix[adj_b[j], adj_a[j]] = weight_array[j];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                float opop = dijkstra(adj_matrix, i, n);
+                count[i] = (n - 1) / (dijkstra(adj_matrix, i, n)); // this is the radius
+            }
+
+            int[,] location = new int[22, 2] { { 500, 100 }, { 500, 200 }, { 950, 100 }, { 950, 200 }, { 700, 550 }, { 900, 500 }, { 550, 250 }, {550, 500}, {550, 430}, {910, 420},
+                                               {730, 300}, {740, 400}, { 630, 100 }, { 690, 200 }, { 640, 400 }, { 800, 200 }, { 840, 550 }, { 800, 450 }, { 800, 250 }, {860, 50},
+                                               {600, 50}, {600, 300}};
+
+            for (int i = 0; i < m; i++)
+            {
+                if (count[adj_a[i]] < 0.01 && count[adj_b[i]] < 0.01)
+                {
+                    draw_2_nodes_and_line(location[adj_a[i], 0], location[adj_a[i], 1], location[adj_b[i], 0], location[adj_b[i], 1], (int)(count[adj_a[i]] * 10000), (int)(count[adj_b[i]] * 10000), adj_a[i], adj_b[i], weight_array[i]);
+                }
+                else
+                {
+                    draw_2_nodes_and_line(location[adj_a[i], 0], location[adj_a[i], 1], location[adj_b[i], 0], location[adj_b[i], 1], (int)(count[adj_a[i]] * 100), (int)(count[adj_b[i]] * 100), adj_a[i], adj_b[i], weight_array[i]);
+                }
+            }
+
+            //display centrality in a text box
+            string str = "";
+            for (int o = 0; o < n; o++)
+            {
+                str += "Node" + " " + o.ToString() + " --->" + count[o] + " | ";
+            }
+            textBox3.Text = str;
+        }
+       
+
+        /*--------------------------------------Drawing Functions----------------------------------------------------*/
+        void draw_circle(int x1, int y1, int r1, int node)
+        {
+            Pen p = new Pen(Color.PaleVioletRed, 2);
+            this.CreateGraphics().DrawEllipse(p, x1, y1, r1, r1);
+            this.CreateGraphics().FillEllipse(Brushes.PaleVioletRed, x1, y1, r1, r1);
+            this.CreateGraphics().DrawString(node.ToString(), new Font("Tahoma", 10), Brushes.Black, x1, y1);
+           
+        }
+        void draw_line(int x1, int y1, int x2, int y2, int weight)
+        {
+            Pen p = new Pen(Color.PaleGoldenrod, 5);
+            this.CreateGraphics().DrawLine(p, x1, y1, x2, y2);
+            this.CreateGraphics().DrawString(weight.ToString(), new Font("Tahoma", 10), Brushes.Black, ((x1 + x2) / 2) - 20, ((y1 + y2) / 2));
+
+        }
+        void draw_2_nodes_and_line(int num_1, int num_2, int num_3, int num_4, int r1, int r2, int node1, int node2, int weight)
+        {
+            draw_line(num_1 + 10, num_2 + 10, num_3 + 10, num_4 + 10, weight);
+            draw_circle(num_1, num_2, r1, node1);
+            draw_circle(num_3, num_4, r2, node2);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /*-----------------------------------Dijkstra's Algorithm-----------------------------------------------*/
+        int minDistance(int[] dist, bool[] sptset, int V)
+        {
+            // Initialize min value
+
+            int min = int.MaxValue, min_index = 0;
+
+            for (int v = 0; v < V; v++)
+                if (sptset[v] == false && dist[v] <= min)
+                {
+                    min = dist[v];
+                    min_index = v;
+                }
+
+            return min_index;
+        }
+
+        float dijkstra(int[,] graph, int src, int V)
+        {
+            float sum_of_dist = 0;
+            int[] dist = new int[V]; 
+            bool[] sptSet = new bool[V]; 
+
+            for (int i = 0; i < V; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+            // Distance of source vertex from itself is always 0
+            dist[src] = 0;
+
+            // Find shortest path for all vertices
+            for (int count = 0; count < V - 1; count++)
+            {
+                int u = minDistance(dist, sptSet, V);
+
+                // Mark the picked vertex as processed
+                sptSet[u] = true;
+
+                // Update dist value of the adjacent vertices of the picked vertex.
+                for (int v = 0; v < V; v++)
+                {
+                    if (!sptSet[v] && Convert.ToBoolean(graph[u, v]) && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+                    {
+                        dist[v] = dist[u] + graph[u, v];
+                    }
+                }
+            }
+            
+
+            for (int i = 0; i < V; i++)
+            {
+                sum_of_dist = sum_of_dist + dist[i];
+            }
+
+            return sum_of_dist;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /*----------------------------------------------------------------------------------*/
+
+    }
+}
